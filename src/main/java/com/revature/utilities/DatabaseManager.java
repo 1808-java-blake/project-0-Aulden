5b8916b14f4db0ea1321d8e0b414770e213caa8c
Aulden <aulden.harris@gmail.com>
2018-08-17 14:44:56 -0400

    Moved DB to RDS on the AWS cloud. Password removed for security

--- src/main/java/com/revature/utilities/DatabaseManager.java
+++ src/main/java/com/revature/utilities/DatabaseManager.java
@@ -6,13 +6,13 @@ import java.sql.SQLException;
 
 public class DatabaseManager {
 	private static final String username = "postgres";
-	private static final String password = "224128";
+	private static final String password = "";
 	private static Connection c = null;
 	
 	public static Connection getConnection(){
 		try {
 			Class.forName("org.postgresql.Driver");
-			c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?currentSchema=big_bank", DatabaseManager.username, DatabaseManager.password);
+			c = DriverManager.getConnection("jdbc:postgresql://revature-1808.clnjocmokdqy.us-west-2.rds.amazonaws.com:5432/postgres?currentSchema=big_bank", DatabaseManager.username, DatabaseManager.password);
 		}
 		catch(SQLException ex) {
 			ex.printStackTrace();
